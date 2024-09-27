7f16f9996f203b8a3c73e0edceb9f26d7643e7b6
SowonBae <engel5526@naver.com>
2024-09-27 16:14:17 +0900

    add backend for develop

---
+++ backend/src/main/java/com/pjt/planit/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.pjt.planit.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000") // Vite 서버 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
+
