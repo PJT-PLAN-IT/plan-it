b82e2d00c704775ca6d652d566687e1660f88546
mossybeach <lyyoon825@gmail.com>
2024-09-28 18:42:52 +0900

    add frontend/ Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,36 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-  // front back 연동 확인용-----확인 했으면 지우기!
-  // back 에는 get방식의 /server/test api 요청을 받을 수 있는 controller가 있어야함.(testcontroller에 기제되어있음)
-  // /api는 vite.config.js에 설정된 것임.
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Header } from "./pages/Header";
+// front back 연동 확인용-----확인 했으면 지우기!
+// back 에는 get방식의 /server/test api 요청을 받을 수 있는 controller가 있어야함.(testcontroller에 기제되어있음)
+// /api는 vite.config.js에 설정된 것임.
 
 function App() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState("");
   useEffect(() => {
-    axios.get('/api/server/test')
-      .then(response => {
+    axios
+      .get("/api/server/test")
+      .then((response) => {
         console.log(response);
         setData(response.data);
       })
-      .catch(error => {
-        console.error("API 요청 에러:", error.response ? error.response.data : error.message);
+      .catch((error) => {
+        console.error(
+          "API 요청 에러:",
+          error.response ? error.response.data : error.message
+        );
       });
   }, []);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        받은 데이터 확인 : {data}
-      </h1>
+      <div className="mx-[300px]">
+        <Header />
+        <br></br>
+        <h1 className="text-3xl font-bold underline">
+          받은 데이터 확인 : {data}
+        </h1>
+      </div>
     </>
   );
 }
