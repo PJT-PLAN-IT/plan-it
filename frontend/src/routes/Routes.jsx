dcd3451e1b2aea5409509c08cb90dd7974cfb38d
hyejin7383 <gpwls553@naver.com>
2024-10-03 19:22:48 +0900

    > add 나의 페이지 작성한 메이트글

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Join from "../pages/Join.jsx";
 import Login from "../pages/Login.jsx";
 import EditUserInfo from "../pages/EditUserInfo.jsx";
+import MyPage from "../pages/MyPage.jsx";
 
 function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ function AppRoutes() {
             <Route path="/join" element={<Join />} />
             <Route path="/login" element={<Login/>} />
             <Route path="/editUserInfo/:custNo" element={<EditUserInfo/>}  />
+            <Route path="/mypage/:custNo" element={<MyPage/>} />
         </Routes>
     );
 }
