5c0dce598ce6307133b23677cad71a759be30a97
SowonBae <engel5526@naver.com>
2024-10-18 14:51:30 +0900

    fix login header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { useLocation } from "react-router-dom";
 
 function App() {
   const location = useLocation();
-  const noHeaderRoutes = ["/plan"];
+  const noHeaderRoutes = ["/plan", "/login"];
 
   return (
     <>
